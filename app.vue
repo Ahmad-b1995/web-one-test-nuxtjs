66f0af0e9e59a0c1afc25b99d60f4f55519466dd
Ahmad Baghereslami <ahmad.b1995@gmail.com>
2024-12-31 16:52:21 +0330

    seo title & description tag

--- app.vue
+++ app.vue
@@ -4,11 +4,29 @@
   </div>
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+    style: 'font-size: 13px'
+  }
+})
+</script>
+
 <style>
-*{
+* {
   direction: rtl;
 }
-img{
+
+img {
   max-width: 100%;
 }
 </style>
